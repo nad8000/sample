6841e547db77f9d5d997351414262a8416e58b36
nad8000 <get_shop_coin@yahoo.co.jp>
2023-04-11 23:51:34 +0900

    styled-componentsの導入、一部のCSSを置き換え

--- sample-client/src/App.tsx
+++ sample-client/src/App.tsx
@@ -1,54 +1,65 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import styled from 'styled-components';
+
+const Header = styled.header`
+min-height: 100vh;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+font-size: calc(10px + 2vmin);
+`;
+
+const Link = styled.a`
+color: rgb(112, 76, 182);
+`;
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header>
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <span>
           <span>Learn </span>
-          <a
-            className="App-link"
+          <Link
             href="https://reactjs.org/"
             target="_blank"
             rel="noopener noreferrer"
           >
             React
-          </a>
+          </Link>
           <span>, </span>
-          <a
-            className="App-link"
+          <Link
             href="https://redux.js.org/"
             target="_blank"
             rel="noopener noreferrer"
           >
             Redux
-          </a>
+          </Link>
           <span>, </span>
-          <a
-            className="App-link"
+          <Link
             href="https://redux-toolkit.js.org/"
             target="_blank"
             rel="noopener noreferrer"
           >
             Redux Toolkit
-          </a>
+          </Link>
           ,<span> and </span>
-          <a
-            className="App-link"
+          <Link
             href="https://react-redux.js.org/"
             target="_blank"
             rel="noopener noreferrer"
           >
             React Redux
-          </a>
+          </Link>
         </span>
-      </header>
+      </Header>
     </div>
   );
 }
