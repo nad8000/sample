4c80afc13585402ba2df80107cc2f944afba19f1
nad8000 <get_shop_coin@yahoo.co.jp>
2023-04-14 00:07:13 +0900

    DB接続

--- sample-server/src/handlers.ts
+++ sample-server/src/handlers.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from 'express';
+import { fetchTags } from '../db';
 
 export const rootHandler = (req: Request, res: Response) => {
   res.send('Hello World! sample server!');
 };
 
 export const sampleHandler = (req: Request, res: Response) => {
-  res.send('Hello World! sample page!');
+  const Tags = fetchTags();
+  res.send(Tags);
 };
