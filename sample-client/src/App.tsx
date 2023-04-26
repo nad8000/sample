1d509ac04ee423f5491cc4bf8970fc9561187552
nad8000 <get_shop_coin@yahoo.co.jp>
2023-04-27 01:08:27 +0900

    ルーティングの作成

--- sample-client/src/App.tsx
+++ sample-client/src/App.tsx
@@ -1,50 +1,20 @@
-import React from 'react';
-import { Counter } from './features/counter/Counter';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import CounterComponent from './features/counter/CounterComponent';
 import './App.css';
-import styled from 'styled-components';
-
-const Header = styled.header`
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-`;
-
-const Link = styled.a`
-  color: rgb(112, 76, 182);
-`; 
 
 function App() {
   return (
-    <div className="App">
-      <Header>
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <Link href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
-            React
-          </Link>
-          <span>, </span>
-          <Link href="https://redux.js.org/" target="_blank" rel="noopener noreferrer">
-            Redux
-          </Link>
-          <span>, </span>
-          <Link href="https://redux-toolkit.js.org/" target="_blank" rel="noopener noreferrer">
-            Redux Toolkit
-          </Link>
-          ,<span> and </span>
-          <Link href="https://react-redux.js.org/" target="_blank" rel="noopener noreferrer">
-            React Redux
-          </Link>
-        </span>
-      </Header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CounterComponent />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
+}
+
+function About() {
+  return <h2>About</h2>;
 }
 
 export default App;
